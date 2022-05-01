1552f6462a4670db2c0f9ac57d458b1565e12615
troy <hunter007nz@gmail.com>
2022-05-02 02:46:16 +1200

    changed a few things

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,7 @@
+// const forgotPasswordTemplate = require('./email-templates/forgot-password.html');
+
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '4b900514949d79800c3c0bec749fd6be'),
-  },
+  }
 });
---
+++ config/email-templates/forgot-password.js
@@ -0,0 +1,14 @@
+const subject = `Reset password`;
+
+const html = `<p>Hi <%= user.firstname %></p>
+<p>Sorry you lost your password. You can click here to reset it: <%= url %></p>`;
+
+const text = `Hi <%= user.firstname %>
+Sorry you lost your password. You can click here to reset it: <%= url %>`;
+
+module.exports = {
+  subject,
+  text,
+  html,
+};
+ 
